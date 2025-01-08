799f99366194b3e93120488463c28a7e04ad32be
johnhilarious <johnhilarious1992@gmail.com>
2025-01-07 23:24:47 -0500

    fulltext change

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -135,7 +135,7 @@ const RoadmapContent = () => {
 
 const Index = () => {
   const [displayText, setDisplayText] = useState("");
-  const fullText = "$buttcheeks completes the H🍩ly Trinity of crypto. \n Here to cover your $butthole and make your $farts roar, $buttcheeks is the most undervalued opportunity of the year (but not for long). \n We're working on a marketplace that lets you share your cheeks and win the finest audience. With {BILLION} of times the utility of $butthole, we're about to leave their butts to dust and start clapping. Join the movement, relax, and become part of the soon-to-be cheekiest community in crypto. Let's moon those $buttcheeks together — we're about to release greatness!";
+  const fullText = "$buttcheeks completes the H🍩ly Trinity of crypto. \n Here to cover your $butthole and make your $farts roar, $buttcheeks is the cheekiest opportunity of your life time. \n  Join the movement, relax, and let's moon those $buttcheeks together!  PS: We're also working on a marketplace that lets you share your cheeks and win the finest audience, that's a {BILLION} of times the utility of $butthole. We're about to leave their asses to ashes and start clapping!";
   const CONTRACT_ADDRESS = "DGjXz3xPKiBKr8sLtpDJmRgYXgMDEKtiKPzHzyuXpump";
   
   useEffect(() => {
